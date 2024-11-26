0bfb251418af7aa9a98d26a802580209ad2cbbac
vitor gabriel da silva pereira <gabrielsorriso666@gmail.com>
2024-11-26 15:34:21 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+namespace Curso2 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+            r.largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            r.altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Area: "+ r.Area().ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro: "+ r.Perimetro().ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal: "+ r.Diagonal().ToString("F2",CultureInfo.InvariantCulture));
+          
+        }
+    }
+}
---
+++ Retangulo.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Curso2 {
+    internal class Retangulo {
+
+        public double largura;
+        public double altura;
+
+        public double Area() {
+            return largura * altura;
+        }
+
+        public double Perimetro() {
+            return 2 * (altura + largura);
+        }
+        public double Diagonal() {
+            return Math.Sqrt(largura * largura + altura * altura);
+        }
+    }
+}
